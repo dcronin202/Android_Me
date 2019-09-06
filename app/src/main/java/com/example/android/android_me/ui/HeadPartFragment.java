e67a46190e1a54bf71c3efdd6353bdb157eceeaa
Dan C <dcro202@hotmail.com>
2019-09-06 15:40:28 -0500

    Completed TFragments.02 exercise

---
+++ app/src/main/java/com/example/android/android_me/ui/HeadPartFragment.java
@@ -0,0 +1,34 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+public class HeadPartFragment extends Fragment {
+
+    public HeadPartFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the Android-Me fragment layout
+        View rootView = inflater.inflate(R.layout.fragment_head_part, container, false);
+
+        // Get a reference to the ImageView in the fragment layout
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.head_part_image_view);
+
+        // Set the image to the first in our list of head images
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+        // Return the rootView
+        return rootView;
+    }
+
+}
